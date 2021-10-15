8a6ce7bbf39b45caa02c7682057b8b083f0110d4
JosyRamos <88045895+JosyRamos@users.noreply.github.com>
2021-10-15 11:06:42 -0400

    NovoRegistro05

--- novoarquivo.js
+++ novoarquivo.js
@@ -1,26 +1,41 @@
+var cadastros, index;
 
-var cadastro,index;
+function cadastrar(nome, idade,sexo){
 
-function cadastrar(numero,nome, idade,sexo,opcoes){
-
-    var cadastro=document.getElementById('tbcadastro');
-    var qtdLinhas = cadastro.rows.length;
-    var linha = cadastro.insertRow(qtdLinhas);
+    cadastros =document.getElementById('thcadastro');
+    var qtdLinhas =cadastros.rows.length;
+    var linha = cadastros.insertRow(qtdLinhas);
     var linhaParam;
 
+    var cellNumero = linha.insertCell(0);
+    var cellNome = linha.insertCell(1);
+    var cellIdade = linha.insertCell(2);
+    var cellSexo = linha.insertCell(3);
 
-    var cellnumero = linha.insertCell(0);
-    var cellnome = linha.insertCell(1);
-    var cellidade = linha.insertCell(2);
-    var cellsexo = linha.insertCell(3);
-    var cellopcoes = linha.insertCell(4);
+    cellNumero.innerHTML = qtdLinhas;
+    cellNome.innerHTML = nome;
+    cellIdade.innerHTML = idade;
+    cellSexo.innerHTML = sexo;
 
-    cellnumero.innerHTML = qtdLinhas;
-    cellnome.innerHTML = nome;
-    cellidade.innerHTML = idade;
-    cellsexo.innerHTML = sexo;
-    cellopcoes.innerHTML = opcoes;
+    preencheCamposForm();
+}
+function altcadastros(nome, idade, sexo){
+    
+    cadastros.rows[index].cells[1].innerHTML = nome;
+    cadastros.rows[index].cells[2].innerHTML = idade;
+    cadastros.rows[index].cells[3].innerHTML = sexo;
 
-    preenchCamposForm();
+}
+function preencheCamposForm(){
+    for(var i=0;i<cadastros.rows.length; i++){
+        cadastros.rows[i].onclick = function(){
 
+            index = this.rowIndex;
+            document.getElementById("txtNome").value = cadastros.rows[index].cells[1].innerText;
+            document.getElementById("txtIdade").value = cadastros.rows[index].cells[2].innerText;
+            document.getElementById("txtSexo").value = cadastros.rows[index].cells[3].innerText;
+        }
+
+
+    }
 }
