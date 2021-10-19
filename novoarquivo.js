43d5773b419fd01b8d34f278f1a25bc132d7a71a
JosyRamos <josileiaramos97@gmail.com>
2021-10-19 17:47:16 -0400

    Finalizado01

--- novoarquivo.js
+++ novoarquivo.js
@@ -1,6 +1,10 @@
 var cadastros, index;
+function soltar(){
+    this.modal();
+}
 
 function cadastrar(nome, idade, sexo, opcoes) {
+
 
     cadastros = document.getElementById('thcadastro');
     var qtdLinhas = cadastros.rows.length;
@@ -19,9 +23,10 @@ function cadastrar(nome, idade, sexo, opcoes) {
 
     let imgEdit = document.createElement("img");
     imgEdit.src = 'img/editar.svg.svg';
+    imgEdit.setAttribute(`onclick`, `editCadastros(` + index + `)`);
     let imgDelet = document.createElement("img");
     imgDelet.src = 'img/excluir.svg.svg';
-    imgDelet.setAttribute(`onclick`, `deleteCadastro(` + qtdLinhas + `)`);
+    imgDelet.setAttribute(`onclick`, `deleteCadastro(` + index + `)`);
     cellOpcoes.appendChild(imgEdit);
     cellOpcoes.appendChild(imgDelet);
 
@@ -29,8 +34,22 @@ function cadastrar(nome, idade, sexo, opcoes) {
     document.getElementById("txtIdade").value = "";
     document.getElementById("slSexo").value = "";
 
-    preencheCamposForm();
+     preencheCamposForm();
+
 }
+function editCadastros(nome, idade, sexo, opcoes) {
+
+    for (var i = 0; i < this.cadastros.rows.length; i++) {
+        cadastros.rows[i].onclick = function () {
+            index = this.rowIndex;
+            document.getElementById("txtNome").value = cadastros.rows[index].cells[1].innerText;
+            document.getElementById("txtIdade").value = cadastros.rows[index].cells[2].innerText;
+            document.getElementById("slSexo").value = cadastros.rows[index].cells[3].innerText;
+        }
+       
+    }
+}
+
 function editarCadastros(nome, idade, sexo, opcoes) {
 
     cadastros.rows[index].cells[1].innerHTML = nome;
@@ -42,9 +61,7 @@ function preencheCamposForm() {
     for (var i = 0; i < this.cadastros.rows.length; i++) {
         cadastros.rows[i].onclick = function () {
             index = this.rowIndex;
-            document.getElementById("txtNome").value = cadastros.rows[index].cells[1].innerText;
-            document.getElementById("txtIdade").value = cadastros.rows[index].cells[2].innerText;
-            document.getElementById("slSexo").value = cadastros.rows[index].cells[3].innerText;
+
         }
 
 
@@ -52,22 +69,63 @@ function preencheCamposForm() {
 }
 
 function deleteCadastro() {
-    
+
     for (var i = 0; i < cadastros.rows.length; i++) {
-        if (index == 1) {
+        if (index == i) {
             cadastros.deleteRow(index);
-            
+
+            document.getElementById("txtNome").value = "";
+            document.getElementById("txtIdade").value = "";
+            document.getElementById("slSexo").value = "";
             return;
         }
     }
 
 
-    //}
-    //function cancelarCadastro(nome,idade,sexo){
+}
+function cancelarCadastro(nome, idade, sexo) {
 
-    //document.getElementById("txtNome").value = "";
-    //document.getElementById("txtIdade").value = "";
-    // document.getElementById("slSexo").value = "";
-    // return;
+    document.getElementById("txtNome").value = "";
+    document.getElementById("txtIdade").value = "";
+    document.getElementById("slSexo").value = "";
+    return;
 
+}
+function modal(){
+    let modal = document.querySelector(".modal");
+    modal.style.display="block";
+    /*cadastros = document.getElementById('thcadastro');
+    var qtdLinhas = cadastros.rows.length;
+    var linha = cadastros.insertRow(qtdLinhas);
+
+    var cellNumero = linha.insertCell(0);
+    var cellNome = linha.insertCell(1);
+    var cellIdade = linha.insertCell(2);
+    var cellSexo = linha.insertCell(3);
+    var cellOpcoes = linha.insertCell(4);
+
+    cellNumero.innerHTML = qtdLinhas;
+    cellNome.innerHTML = nome;
+    cellIdade.innerHTML = idade;
+    cellSexo.innerHTML = sexo;
+
+    let imgEdit = document.createElement("img");
+    imgEdit.src = 'img/editar.svg.svg';
+    imgEdit.setAttribute(`onclick`, `editCadastros(` + index + `)`);
+    let imgDelet = document.createElement("img");
+    imgDelet.src = 'img/excluir.svg.svg';
+    imgDelet.setAttribute(`onclick`, `deleteCadastro(` + index + `)`);
+    cellOpcoes.appendChild(imgEdit);
+    cellOpcoes.appendChild(imgDelet);
+
+    document.getElementById("txtNome").value = "";
+    document.getElementById("txtIdade").value = "";
+    document.getElementById("slSexo").value = "";
+
+     preencheCamposForm();*/
+
+}
+function fechar(){
+    let modal = document.querySelector(".modal");
+    modal.style.display="none";
 }
